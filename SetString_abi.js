64ffe8eb695a7b7dacff3d01ef69796a6b7074c2
Jagger Villalobos <jagger.vill@gmail.com>
2020-10-19 18:40:10 -0400

    ABI for our current contract
    JS version

---
+++ SetString_abi.js
@@ -0,0 +1,41 @@
+var SetString = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_string",
+                "type": "string"
+            }
+        ],
+        "name": "set",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "get",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "name": "_initstring",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    }
+]
